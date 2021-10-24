0998f3184f25f1410501093b9140e064309bcb2e
Mihail Gaberov <mihail.gaberov@gmail.com>
2021-10-24 16:22:55 +0200

    :lipstick: Styled Reset Filters button

--- src/components/FiltersContainer/styles.tsx
+++ src/components/FiltersContainer/styles.tsx
@@ -5,4 +5,12 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: space-evenly;
   height: 20em;
+  
+  button {
+    background: none;
+    border: none;
+    color: darkgray;
+    text-align: left;
+    cursor: pointer;
+  }
 `
