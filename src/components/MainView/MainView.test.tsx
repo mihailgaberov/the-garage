eec139dc58dcba6593c8f8995fe6f40e404f247b
mihailgaberov <mihail.gaberov@gmail.com>
2021-10-25 10:28:17 +0200

    :white_check_mark: Added tests.

--- src/components/MainView/MainView.test.tsx
+++ src/components/MainView/MainView.test.tsx
@@ -7,6 +7,12 @@ test('renders MainView component containing child components inside', () => {
   render(<MainView />);
   const txtLevelsElement = screen.getByText(/LEVELS/i);
   expect(txtLevelsElement).toBeInTheDocument();
-  const txtVehiclesElement = screen.getByText(/VEHICLES/i);
-  expect(txtVehiclesElement).toBeInTheDocument();
+  const txtResetFilters = screen.getByText(/Reset Filters/i);
+  expect(txtResetFilters).toBeInTheDocument();
+  const txtType = screen.getByText(/TYPE/i);
+  expect(txtType).toBeInTheDocument();
+  const txtCar = screen.getByText(/Car/i);
+  expect(txtCar).toBeInTheDocument();
+  const txtMotorbike = screen.getByText(/Motorbike/i);
+  expect(txtMotorbike).toBeInTheDocument();
 });
--- src/components/VehiclesList/VehiclesList.test.tsx
+++ src/components/VehiclesList/VehiclesList.test.tsx
@@ -3,8 +3,31 @@ import { render, screen } from '@testing-library/react';
 
 import VehiclesList from './index';
 
-test('renders VehiclesList component', () => {
+test('renders VehiclesList component correctly when no data is loaded', () => {
   render(<VehiclesList />);
-  const txtElement = screen.getByText(/VEHICLES/i);
+  const txtElement = screen.getByText(/No data./i);
   expect(txtElement).toBeInTheDocument();
+});
+
+test('renders VehiclesList component correctly with data', () => {
+  render(<VehiclesList vehicles={[
+    {
+      "licenseNumber": "XBZ 120",
+      "vehicleType": "Car",
+      "slotNumber": 1,
+      "levelNumber": 1
+    },
+    {
+      "licenseNumber": "XBZ 121",
+      "vehicleType": "Motorbike",
+      "slotNumber": 2,
+      "levelNumber": 1
+    }
+  ]} />);
+  const txtVehicles = screen.getByText(/Vehicles/i);
+  expect(txtVehicles).toBeInTheDocument();
+  const txtSlots = screen.getAllByText(/Slot:/i);
+  expect(txtSlots.length).toEqual(2);
+  const txtPlateNumber = screen.getByText(/XBZ 120/i);
+  expect(txtPlateNumber).toBeInTheDocument();
 });
